b132a4e268c389d68074387ee304ca32fd289e72
chandrika <chandrikabiswal1@gmail.com>
2020-05-16 12:01:19 +0530

    Updates

---
+++ precache-manifest.f8f819b0232216b24d614d074e49d0d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608217174fd86f63699b239f531c8f93",
+    "url": "/reactstrap-card/index.html"
+  },
+  {
+    "revision": "54179ef3165f90a02e0a",
+    "url": "/reactstrap-card/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "27d81ddcbb4f8d48a220",
+    "url": "/reactstrap-card/static/css/main.2d861942.chunk.css"
+  },
+  {
+    "revision": "54179ef3165f90a02e0a",
+    "url": "/reactstrap-card/static/js/2.f337dfe7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactstrap-card/static/js/2.f337dfe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d81ddcbb4f8d48a220",
+    "url": "/reactstrap-card/static/js/main.772c46fd.chunk.js"
+  },
+  {
+    "revision": "0f11d5884695de631e87",
+    "url": "/reactstrap-card/static/js/runtime-main.d23e0fb8.js"
+  },
+  {
+    "revision": "a399e1d054c0ecc62daaca42dc1a4787",
+    "url": "/reactstrap-card/static/media/256x186.a399e1d0.svg"
+  },
+  {
+    "revision": "d5ff1945241b0976e3cf03a84d4444dd",
+    "url": "/reactstrap-card/static/media/318x180.d5ff1945.svg"
+  }
+]);
