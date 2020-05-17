4bb7b578140e91d35640639113942b1f2e826f2a
ch_biswal <chandrikabiswal1@gmail.com>
2020-05-18 01:29:37 +0530

    Updated LandingPage.js
    1.Updated LandingPage
    2.Created Components folder
    3.Inside component folder Hero.js ,Banner.js file created
    4.Updated App.css

---
+++ pages/LandingPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+
+const home = () => {
+  return (
+    <>
+      <Hero>
+        <Banner
+          title="Innovation Story Landing"
+          subtitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+                   
+        >
+         
+        </Banner>
+      </Hero>
+      
+      
+    </>
+  );
+};
+
+export default home;
