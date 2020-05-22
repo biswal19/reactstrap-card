c268e1390f33fb589d19e4f1a04217602ef229d1
ch_biswal <chandrikabiswal1@gmail.com>
2020-05-22 20:45:23 +0530

    Update App.js
    1.Update the App.js file.

--- src/App.js
+++ src/App.js
@@ -5,24 +5,37 @@ import {
   CardTitle, Button,
   Row, Col
 } from 'reactstrap';
-import img1 from './assets/image/318x180.svg'
+import img1 from './assets/image/318x180.mp4'
 import img2 from './assets/image/256x186.svg'
 import TopNav from './Nav';
-import LandingPage from './LandingPage';
+import LandingPage from './pages/LandingPage';
 import Footer from "./Footer";
+
+
+
 
 function App() {
   return (
     <div className="card-text-center">
-    
       <div className="overflow">
       <TopNav />
       <LandingPage />  
-      
-     
-        <CardDeck>
+      <CardDeck>
+         
           <Card>
-          <CardImg top width="100%" src={img1} alt="Image 1" />
+          <iframe width="595"height="330" src="https://www.youtube.com/embed/w01V5FI03MQ" frameborder="0"></iframe>
+            <CardImg top width="50%" src={img1} alt=" "/>
+            
+            <CardBody>
+              <CardTitle>Tier 1 Story Headline</CardTitle>
+              <CardText>This is a wider card with supporting text.</CardText>
+              <Button>View</Button>
+            </CardBody>
+          
+          </Card>
+          <Card>
+          <iframe width="595" height="330" src="https://www.youtube.com/embed/CP9vIFHX9hk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <CardImg top width="100%" src={img1} alt=" " />
             <CardBody>
               <CardTitle>Tier 1 Story Headline</CardTitle>
               <CardText>This is a wider card with supporting text.</CardText>
@@ -30,15 +43,8 @@ function App() {
             </CardBody>
           </Card>
           <Card>
-          <CardImg top width="100%" src={img1} alt="Image 1" />
-            <CardBody>
-              <CardTitle>Tier 1 Story Headline</CardTitle>
-              <CardText>This is a wider card with supporting text.</CardText>
-              <Button>View</Button>
-            </CardBody>
-          </Card>
-          <Card>
-          <CardImg top width="100%" src={img1} alt="Image 1" />
+          <iframe width="595" height="330" src="https://www.youtube.com/embed/c1aEJ38GMZY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <CardImg top width="100%" src={img1} alt=" " />
             <CardBody>
               <CardTitle>Tier 1 Story Headline</CardTitle>
               <CardText>This is a wider card with supporting text.</CardText>
@@ -48,7 +54,8 @@ function App() {
         </CardDeck>
         <CardDeck>
           <Card>
-          <CardImg top width="100%" src={img1} alt="Image 1" />
+          <iframe width="595" height="330" src="https://www.youtube.com/embed/87y4GDIrHM0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <CardImg top width="100%" src={img1} alt=" " />
             <CardBody>
               <CardTitle>Tier 1 Story Headline</CardTitle>
               <CardText>This is a wider card with supporting text.</CardText>
@@ -56,7 +63,8 @@ function App() {
             </CardBody>
           </Card>
           <Card>
-          <CardImg top width="100%" src={img1} alt="Image 1" />
+          <iframe width="595" height="330" src="https://www.youtube.com/embed/ixnRvQuCE6s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <CardImg top width="100%" src={img1} alt=" " />
             <CardBody>
               <CardTitle>Tier 1 Story Headline</CardTitle>
               <CardText>This is a wider card with supporting text.</CardText>
@@ -64,7 +72,8 @@ function App() {
             </CardBody>
           </Card>
           <Card>
-          <CardImg top width="100%" src={img1} alt="Image 1" />
+          <iframe width="595" height="330" src="https://www.youtube.com/embed/ZSt9tm3RoUU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <CardImg top width="100%" src={img1} alt=" " />
             <CardBody>
               <CardTitle>Tier 1 Story Headline</CardTitle>
               <CardText>This is a wider card with supporting text.</CardText>
@@ -263,6 +272,7 @@ function App() {
                     Some quick example text to build on the card title.
                   </CardText>
                   <Button>View</Button>
+                  
                 </CardBody>
               </Col>
             </Row>
